8ff9865b37ee4a5cccb3e5cff73616c4e81e19dd
Rose Tucker <rtucker@thoughtworks.com>
2017-06-15 19:51:08 +0100

    Say hello world

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.instructions}>
+      Hello World
+    </Text>
+  </View>
+);
+
+export default App;
